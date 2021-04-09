8063b7d4a9648003f61842f0e3aefba585f3492e
Marcos Paulo <marcos.p.mcruz@outlook.com>
2021-04-08 22:46:56 -0300

    concatenando number com string

--- python_basico/number/primeiro_number.py
+++ python_basico/number/primeiro_number.py
@@ -1,10 +1,13 @@
 num_a = 10
 num_b = 2
 
+soma = num_a + num_b
+
 print(num_a + num_b)
 print(num_a - num_b)
 print(num_a * num_b)
 print(num_a / num_b)
 print(num_a % num_b)
 print(num_a ** num_b)
-print(num_a.)
+
+print("A soma de 10 + 2 é " + str(soma))
