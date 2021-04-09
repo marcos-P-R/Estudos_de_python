a4251db8460691a8911d9cb2cf6d0cba051288f5
Marcos Paulo <marcos.p.mcruz@outlook.com>
2021-04-08 22:29:46 -0300

    aprendendo uso de string e usando alguns metodos.

---
+++ python_basico/string/primeiro_exemplo.py
@@ -0,0 +1,5 @@
+author = "tomás de aquino"
+livros = ["Suma Teológica", "O Ente e a Essência", "Suma Contra os Gentios"]
+lista_author = [author.title(), author.lower(), author.upper()]
+print(lista_author)
+print(author.title() + " : " + livros[1])
