461c4008f723910562e226cfa2ca4f6b8c8f59cf
Marcos Paulo <marcos.p.mcruz@outlook.com>
2021-04-08 23:17:00 -0300

    ordenando lista com metodo sort

---
+++ python_basico/listas/segundo_list.py
@@ -0,0 +1,7 @@
+authors = ['John Ronald Reuel Tolkien', 'Philip Pullman', 'Masashi Kishimoto', 'Clive Staples Lewis']
+
+authors.sort()
+
+print(len(authors)) # retorna o tamanho da lista
+
+print(authors)
